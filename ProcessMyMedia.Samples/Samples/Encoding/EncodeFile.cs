fb95fecae60a59b9d0851a9ffa2284a2defa8189
mnicolescu <mnicolescu@WDEHQPAR1-00042>
2018-11-07 11:08:00 +0100

    Encode File task refactoring : Preset argument has now a string type

---
+++ ProcessMyMedia.Samples/Samples/Encoding/EncodeFile.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcessMyMedia.Samples
+{
+    public class EncodeFile
+    {
+        public class EncodeFiletWorkflowData
+        {
+            public string FilePath { get; set; }
+
+            public string Preset { get; set; }
+        }
+    }
+}
