669b6c7e15bf26fe09cfce3c383c19d1f44d65d5
Process My Media <mnicolescu@gmail.com>
2018-11-11 14:50:03 +0100

    Add job priority enum

---
+++ ProcessMyMedia/Model/Job/JobPriority.cs
@@ -0,0 +1,21 @@
+﻿namespace ProcessMyMedia.Model
+{
+    /// <summary>
+    /// Job Priority
+    /// </summary>
+    public enum JobPriority
+    {
+        /// <summary>
+        /// The low
+        /// </summary>
+        Low = 0,
+        /// <summary>
+        /// The normal
+        /// </summary>
+        Normal = 1,
+        /// <summary>
+        /// The high
+        /// </summary>
+        High = 2
+    }
+}
