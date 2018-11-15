7338d04b6a948b0a2a5b7bff29f3de099d3cf60e
mnicolescu <mnicolescu@WDEHQPAR1-00042>
2018-11-15 11:49:47 +0100

    Add CreateLinkedServiceTask

---
+++ ProcessMyMedia/Model/Data/LinkedServiceEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessMyMedia.Model
+{
+    /// <summary>
+    /// Linked Service Entity
+    /// </summary>
+    /// <seealso cref="ProcessMyMedia.Model.DataEntityBase" />
+    public class LinkedServiceEntity : DataEntityBase
+    {
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public string Type { get; set; }
+    }
+}
---
+++ ProcessMyMedia/Tasks/Data/CreateLinkedServiceTask.cs
@@ -0,0 +1,90 @@
+﻿namespace ProcessMyMedia.Tasks.Data
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Logging;
+
+    using WorkflowCore.Interface;
+    using WorkflowCore.Models;
+
+    using ProcessMyMedia.Model;
+    using ProcessMyMedia.Services.Contract;
+
+    public class CreateLinkedServiceTask : DataFactoryTaskBase
+    {
+        /// <summary>
+        /// Gets or sets the linked service to create.
+        /// </summary>
+        /// <value>
+        /// The linked service to create.
+        /// </value>
+        public LinkedServiceEntity LinkedServiceToCreate { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateLinkedServiceTask"/> class.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        /// <param name="loggerFactory">The logger factory.</param>
+        public CreateLinkedServiceTask(IDataFactoryService service, ILoggerFactory loggerFactory) : base(service, loggerFactory)
+        {
+
+        }
+
+        /// <summary>
+        /// Validates the input.
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// LinkedServiceToCreate
+        /// or
+        /// Name
+        /// or
+        /// Type
+        /// </exception>
+        protected override void ValidateInput()
+        {
+            if (this.LinkedServiceToCreate == null)
+            {
+                throw new ArgumentException($"{nameof(this.LinkedServiceToCreate)} is required");
+            }
+
+            if (string.IsNullOrEmpty(this.LinkedServiceToCreate.Name))
+            {
+                throw new ArgumentException($"{nameof(this.LinkedServiceToCreate.Name)} is required for the property {nameof(this.LinkedServiceToCreate)}");
+            }
+
+            if (string.IsNullOrEmpty(this.LinkedServiceToCreate.Type))
+            {
+                throw new ArgumentException($"{nameof(this.LinkedServiceToCreate.Type)} is required for the property {nameof(this.LinkedServiceToCreate)}");
+            }
+        }
+
+
+        /// <summary>
+        /// Runs the media task asynchronous.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        protected async override Task<ExecutionResult> RunTaskAsync(IStepExecutionContext context)
+        {
+            await this.service.CreateOrUpdateLinkedServiceAsync(
+                this.LinkedServiceToCreate.Name,
+                this.LinkedServiceToCreate.Type,
+                this.LinkedServiceToCreate.Properties);
+
+            return ExecutionResult.Next();
+        }
+
+
+
+        /// <summary>
+        /// Cleanups the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        protected async override Task Cleanup(IStepExecutionContext context)
+        {
+            
+        }
+    }
+}
