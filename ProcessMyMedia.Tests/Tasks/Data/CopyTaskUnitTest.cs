581bfcd4e743104512bfed68dc1c920aaf63c543
mnicolescu <mnicolescu@WDEHQPAR1-00042>
2018-11-20 17:40:06 +0100

    Add unit test class for data factory tasks

---
+++ ProcessMyMedia.Tests/Tasks/Data/CopyTaskUnitTest.cs
@@ -0,0 +1,42 @@
+﻿namespace ProcessMyMedia.Tests.Tasks.Data
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using WorkflowCore.Interface;
+
+    using ProcessMyMedia.Model;
+
+    [TestClass]
+    [TestCategory("DataFactory")]
+    public class CopyTaskUnitTest : UnitTestBase<CopyTaskUnitTest.CopyTaskWorkflow, CopyTaskUnitTest.CopyTaskWorkflowData>
+    {
+        public CopyTaskUnitTest()
+        {
+            this.Setup();
+        }
+
+        public class CopyTaskWorkflow : IWorkflow<CopyTaskWorkflowData>
+        {
+            public string Id => nameof(CopyTaskWorkflow);
+
+            public int Version => 1;
+
+            public void Build(IWorkflowBuilder<CopyTaskWorkflowData> builder)
+            {
+                builder
+                    .UseDefaultErrorBehavior(WorkflowCore.Models.WorkflowErrorHandling.Terminate)
+                    .StartWith<ProcessMyMedia.Tasks.CopyTask>()
+                        .Input(task => task.SourcePath, data => data.SourcePath)
+                        .Input(task => task.DestinationPath, data => data.DestinationPath);
+            }
+        }
+
+        public class CopyTaskWorkflowData
+        {
+            public DataPath SourcePath { get; set; }
+
+            public DataPath DestinationPath { get; set; }
+        }
+
+    }
+}
---
+++ ProcessMyMedia.Tests/Tasks/Data/CreateLinkedServiceTaskUnitTest.cs
@@ -0,0 +1,39 @@
+﻿namespace ProcessMyMedia.Tests.Tasks.Data
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using WorkflowCore.Interface;
+
+    using ProcessMyMedia.Model;
+
+    [TestClass]
+    [TestCategory("DataFactory")]
+    public class CreateLinkedServiceTaskUnitTest 
+        : UnitTestBase<CreateLinkedServiceTaskUnitTest.CreateLinkedServiceWorkflow, CreateLinkedServiceTaskUnitTest.CreateLinkedServiceWorkflowData>
+    {
+        public CreateLinkedServiceTaskUnitTest()
+        {
+            this.Setup();
+        }
+
+        public class CreateLinkedServiceWorkflow : IWorkflow<CreateLinkedServiceWorkflowData>
+        {
+            public string Id => nameof(CreateLinkedServiceWorkflow);
+
+            public int Version => 1;
+
+            public void Build(IWorkflowBuilder<CreateLinkedServiceWorkflowData> builder)
+            {
+                builder
+                    .UseDefaultErrorBehavior(WorkflowCore.Models.WorkflowErrorHandling.Terminate)
+                    .StartWith<ProcessMyMedia.Tasks.CreateLinkedServiceTask>()
+                    .Input(task => task.LinkedServiceToCreate, data => data.Server);
+            }
+        }
+
+        public class CreateLinkedServiceWorkflowData
+        {
+            public LinkedServiceEntity Server { get; set; }
+        }
+    }
+}
