21237b4f146d7f3d5785c02334ccdf3d22b8f2f7
mnicolescu <mnicolescu@WDEHQPAR1-00042>
2018-11-21 09:52:14 +0100

    Add unit tests for CreateLinkedServiceTask

--- ProcessMyMedia.Tests/Tasks/Data/CreateLinkedServiceTaskUnitTest.cs
+++ ProcessMyMedia.Tests/Tasks/Data/CreateLinkedServiceTaskUnitTest.cs
@@ -1,11 +1,14 @@
 ﻿namespace ProcessMyMedia.Tests.Tasks.Data
 {
     using System;
+    using System.Threading.Tasks;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     using WorkflowCore.Interface;
     using WorkflowCore.Models;
+
+    using Moq;
 
     using ProcessMyMedia.Model;
 
@@ -36,6 +39,94 @@
             Assert.AreEqual(1, this.UnhandledStepErrors.Count);
             Assert.IsInstanceOfType(this.UnhandledStepErrors[0].Exception, typeof(ArgumentException));
             Assert.IsTrue(this.UnhandledStepErrors[0].Exception.Message.Contains(nameof(ProcessMyMedia.Tasks.CreateLinkedServiceTask.LinkedServiceToCreate)));
+        }
+
+        [TestMethod]
+        public void CreateLinkedServiceWithoutNameTest()
+        {
+            this.dataFactoryService.Setup(mock => mock.AuthAsync()).Throws<NotSupportedException>();
+            this.dataFactoryService.Setup(mock => mock.Dispose()).Throws<NotSupportedException>();
+
+
+            var datas = new CreateLinkedServiceWorkflowData()
+            {
+                Server = new LinkedServiceEntity()
+            };
+
+
+            var workflowId = this.StartWorkflow(datas);
+            WaitForWorkflowToComplete(workflowId, TimeSpan.FromSeconds(30));
+
+            Assert.AreEqual(WorkflowStatus.Terminated, this.GetStatus((workflowId)));
+            Assert.AreEqual(1, this.UnhandledStepErrors.Count);
+            Assert.IsInstanceOfType(this.UnhandledStepErrors[0].Exception, typeof(ArgumentException));
+            Assert.IsTrue(this.UnhandledStepErrors[0].Exception.Message.Contains(nameof(ProcessMyMedia.Tasks.CreateLinkedServiceTask.LinkedServiceToCreate)));
+            Assert.IsTrue(this.UnhandledStepErrors[0].Exception.Message.Contains(nameof(ProcessMyMedia.Tasks.CreateLinkedServiceTask.LinkedServiceToCreate.Name)));
+        }
+
+        [TestMethod]
+        public void CreateLinkedServiceWithoutTypeTest()
+        {
+            this.dataFactoryService.Setup(mock => mock.AuthAsync()).Throws<NotSupportedException>();
+            this.dataFactoryService.Setup(mock => mock.Dispose()).Throws<NotSupportedException>();
+
+
+            var datas = new CreateLinkedServiceWorkflowData()
+            {
+                Server = new LinkedServiceEntity()
+                {
+                    Name = "MyServer"
+                }
+            };
+
+
+            var workflowId = this.StartWorkflow(datas);
+            WaitForWorkflowToComplete(workflowId, TimeSpan.FromSeconds(30));
+
+            Assert.AreEqual(WorkflowStatus.Terminated, this.GetStatus((workflowId)));
+            Assert.AreEqual(1, this.UnhandledStepErrors.Count);
+            Assert.IsInstanceOfType(this.UnhandledStepErrors[0].Exception, typeof(ArgumentException));
+            Assert.IsTrue(this.UnhandledStepErrors[0].Exception.Message.Contains(nameof(ProcessMyMedia.Tasks.CreateLinkedServiceTask.LinkedServiceToCreate)));
+            Assert.IsTrue(this.UnhandledStepErrors[0].Exception.Message.Contains(nameof(ProcessMyMedia.Tasks.CreateLinkedServiceTask.LinkedServiceToCreate.Type)));
+        }
+
+        [TestMethod]
+        public void CreateLinkedServiceTest()
+        {
+            this.dataFactoryService.Setup(mock => mock.AuthAsync())
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+            this.dataFactoryService.Setup(mock => mock.Dispose()).Verifiable();
+
+
+            var datas = new CreateLinkedServiceWorkflowData()
+            {
+                Server = new LinkedServiceEntity()
+                {
+                    Name = "MyServer",
+                    Type = "FtpServer",
+                    TypeProperties = new
+                    {
+                        host = "localhost",
+                        port = 21
+                    }
+                }
+            };
+
+            this.dataFactoryService.Setup(mock => mock.CreateOrUpdateLinkedServiceAsync(
+                It.Is<string>(s => s == datas.Server.Name),
+                It.Is<string>(s => s == datas.Server.Type),
+                It.IsAny<object>()))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+
+            var workflowId = this.StartWorkflow(datas);
+            WaitForWorkflowToComplete(workflowId, TimeSpan.FromSeconds(30));
+
+            Assert.AreEqual(WorkflowStatus.Complete, this.GetStatus((workflowId)));
+
+            this.dataFactoryService.Verify();
         }
 
         public class CreateLinkedServiceWorkflow : IWorkflow<CreateLinkedServiceWorkflowData>
